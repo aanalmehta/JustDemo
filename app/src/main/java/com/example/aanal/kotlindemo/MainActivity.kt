c4e7874aba8c6b645f72ca82dc59192cf9e43d58
aanamehta <aanal.mehta@i-link.co.in>
2018-12-18 17:39:59 +0530

    Layout changes

--- app/src/main/java/com/example/aanal/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/example/aanal/kotlindemo/MainActivity.kt
@@ -25,16 +25,39 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     @TargetApi(Build.VERSION_CODES.KITKAT)
     private fun addAnimationOperations() {
         var set = false
+        var count = 0
         val constraint1 = ConstraintSet()
         constraint1.clone(root)
         val constraint2 = ConstraintSet()
         constraint2.clone(this, R.layout.activity_main_1)
+        val constraint3 = ConstraintSet()
+        constraint3.clone(this, R.layout.activity_main_2)
+        val constraint4 = ConstraintSet()
+        constraint4.clone(this, R.layout.activity_main_3)
         findViewById<ImageView>(R.id.imageView).setOnClickListener{
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                 val autoTransition = AutoTransition()
                 autoTransition.duration = 1000
                 TransitionManager.beginDelayedTransition(root, autoTransition)
-                val constraint = if(set) constraint1 else constraint2
+                var constraint = ConstraintSet()
+                when(count) {
+                    0 -> {
+                        constraint = constraint2
+                        count++
+                    }
+                    1 -> {
+                        constraint = constraint3
+                        count++
+                    }
+                    2 -> {
+                        constraint = constraint4
+                        count++
+                    }
+                    3 -> {
+                        constraint = constraint1
+                        count = 0
+                    }
+                }
                 constraint.applyTo(root)
                 set = !set
             }
