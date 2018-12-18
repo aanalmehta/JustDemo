69eca16fd40e42613321a1fa01a9a807b1425b91
aanamehta <aanal.mehta@i-link.co.in>
2018-12-18 16:55:33 +0530

    Layout changes

--- app/src/main/java/com/example/aanal/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/example/aanal/kotlindemo/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.example.aanal.kotlindemo
 
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintSet
+import android.transition.TransitionManager
+import android.view.View
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View?) {
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addAnimationOperations()
+    }
+
+    fun addAnimationOperations() {
+        var set = false
+        val constraint1 = ConstraintSet()
+        constraint1.clone(root)
+        val constraint2 = ConstraintSet()
+        constraint2.clone(this, R.layout.activity_main_new)
+        findViewById<ImageView>(R.id.imageView).setOnClickListener{
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                TransitionManager.beginDelayedTransition(root)
+                val constraint = if(set) constraint1 else constraint2
+                constraint.applyTo(root)
+                set = !set
+            }
+        }
     }
 }
